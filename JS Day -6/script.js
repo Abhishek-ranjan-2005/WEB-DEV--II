8a46cc7f5b850f0b809e6c1d376a14bda926e9dd
Abhishek Ranajan <abhishekranjan2005@hotmail.com>
2026-01-29 21:19:14 +0530

    Object and freeze

--- JS Day -6/script.js
+++ JS Day -6/script.js
@@ -1,8 +1,52 @@
 let user={
-    fullname: "abhishek ranjan",
-    address: "gurugram",
-    mobile no: 1234567890,
-    favcolour: "black","white","blue"
+    fullname:"abhishek ranjan",
+    address: {
+        city :"gurugram",
+        state: "haryana"
+    },
+    mobileno: 1234567890,
+    favcolour: ["black","white","blue"],
+    demo: function(){
+        return "demo function";
+    }
 }
 
-console.log(user.fullname,user.address,user.mobile no,user.favcolour);
+console.log(user.fullname,user.address.city,user.mobileno,user.favcolour[1],user.demo());
+
+
+
+
+Object methods
+
+// Object.keys(variable name)
+
+// console.log(Object.keys(user))
+
+// console.log(Object.values(user))
+
+// console.log(Object.entries(user));
+
+
+
+
+const car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+
+//  we can`t add a new key and value in pair but
+//  we can change or update existing values
+
+Object.freeze (car);
+
+car.model = "Corolla";
+car.colour = "black";
+car.year = 2021;
+
+console.log(car);
+
+
+
+var date = new Date();
+console.log(date);
